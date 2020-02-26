fd58dbe8c1e6faabe0c706b741454c66408e043a
Stephen Ryall <stevenryall812@gmail.com>
2020-02-26 15:11:05 +0000

    Initial commit

---
+++ server/server.js
@@ -0,0 +1,70 @@
+"use strict";
+//require('./config')
+const express = require('express')
+const app = express()
+const path = require("path");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+require('dotenv').config({ path: '../.env' })
+const jsonfile = require('jsonfile')
+const port = process.env.APP_SERVER_PORT;
+const host = process.env.API_HOST;
+const store = path.join(__dirname, '/store.json')
+const publicPath = path.join(__dirname, "public");
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+
+app.get('/gettodos', (req, res) => {
+    try {
+        jsonfile.readFile(store, (err, obj) => {
+            if (err) console.log("json read error => ", err)
+            res.status(200).send(obj);
+        })
+    } catch (error) {
+        res.status(400).send(error)
+    }
+})
+
+app.post('/addtodo', (req, res) => {
+    try {
+        var todo = res.req.body.todo;
+        jsonfile.readFile(store, (err, obj) => {
+            if (err) console.log("json read error => ", err)
+            obj.todos.push(todo)
+            var augObj = {
+                todos: obj.todos
+            };
+            jsonfile.writeFile(store, augObj, (err) => {
+                if (err) console.error(err)
+                res.status(201).send({ message: "the todo was added!" })
+            })
+        })
+    } catch (error) {
+        console.log("error for adding a todo", error)
+        res.status(400).send(error)
+    }
+})
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(publicPath, "index.html"));
+});
+
+
+app.listen(port, () => console.log(`Running on port: ${port}`));
+
+
+
+/**
+ *
+ *         volumes:
+            - ./server:/srv/app/test-app-server
+            - ./srv/app/test-app-server/node_modules
+
+        volumes:
+            - ./src:/srv/app/test-app-src
+            - ./srv/app/test-app-src/node_modules
+ *
+ */
---
+++ src/webpack.config.js
@@ -0,0 +1,43 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+  entry: {
+    main: path.join(__dirname, '/index.js')
+  },
+  output: {
+    path: path.resolve(__dirname, '../server', 'public'),
+    filename: 'bundle.js',
+    publicPath: '/'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader"
+          }
+        ]
+      }
+    ]
+  },
+  devServer: {
+    contentBase: path.join(__dirname, '../server', "public"),
+    proxy: {
+      "*": {
+        target: "http://server:8000"  //service name specified inside the docker-compose file
+      }
+    },
+    historyApiFallback: true,
+    host: '0.0.0.0',
+    port: 8080
+  }
+};
